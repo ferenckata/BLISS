f4ab204713230a1ba1bf5a69066280af280f8656
ferenckata <35810660+ferenckata@users.noreply.github.com>
2018-02-27 14:40:35 -0800

    initial fastq processing
    barcode filtering, mapping and UMI grouping

---
+++ bliss_main_kf.sh
@@ -0,0 +1,36 @@
+# Processing of the BLISS sequencing results
+# this script can be called as
+# bliss_main_kf.sh infastq inputdir refgenome pydir
+inputdir = $1		# full path to the fastq file
+infastq = $2		# the name of the gz compressed fastq file without the ".gz" ending (e.i. rm31.fastq)
+WL = $3				# the name of the file containing a list of the experiment barcodes (one barcode per line, no header)
+refgenome = $4		# full path to reference genome fastq file or fasta file if it has been indexed already
+pydir = $5			# full path to the mismatchmaker.py file
+q = $6				# mapping quality threshold for filtering
+
+# unzipping the fastq file of interest
+gunzip $inputdir"/"$infastq".gz"
+# retrieving the run ID
+name = $(echo $infastq | cut -d'.' -f1)
+
+# creating all possible mismatches of the experiment barcode
+cat $WL | while read LINE; do python3 $pydir"/"mismatchmaker.py $LINE >> mmWL ;done
+
+# experiment barcode filtering with umi_tools
+umi_tools extract -p NNNNNNNNCCCCCCCC -I $inputdir"/"$infastq --filter-cell-barcode --whitelist=mmWL > $name"_ft.fastq"
+
+# mapping the barcode filtered fastq files
+# bwa index $refseqfq      # only for the first time
+# bwa mem $refseqfa $outfq > $outsam
+bwa mem $refgenome $name"_ft.fastq" > $name"_ft_mp.sam"
+
+# quality filtering, converting, sorting and indexing sam to bam for later use
+samtools view -Sb -q $q $name"_ft_mp.sam" > $name"_ft_mp.bam"
+samtools sort $name"_ft_mp.bam" -o $name"_ft_mp_srt.bam"
+samtools index $name"_ft_mp_srt.bam"
+
+# groupping the UMIs with umi_tools
+umi_tools group -I $name"_ft_mp_srt.bam" --group-out=groups.tsv --per-cell --output-bam -S $name"_ft_mp_srt_gp.bam"
+
+# converting final bam to sam
+samtools view -h -o $name"_ft_mp_srt_gp.sam" $name"_ft_mp_srt_gp.bam"
