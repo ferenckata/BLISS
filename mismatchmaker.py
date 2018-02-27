1394b51ec46a256487a7533e925a78f906b68fd3
ferenckata <35810660+ferenckata@users.noreply.github.com>
2018-02-27 14:50:47 -0800

    commenting

--- mismatchmaker.py
+++ mismatchmaker.py
@@ -1,4 +1,6 @@
-## create all possible mismatches of a sequence
+# create all possible mismatches of a sequence
+# input is the list of all experiment barcodes
+# output is STDOUT
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("seq",help="Sequence to be mismatched", type=str)
@@ -8,9 +10,11 @@ seq = args.seq
 NTPs=['A','C','T','G']
 # mismatching #
 k=0
+# print the original sequence
 print(seq)
 for base in seq:
     k+=1
     for nt in NTPs:
+        # print all sequences that do not match with the original one in only one position
         if base != nt:
             print(seq[0:k-1]+nt+seq[k:len(seq)])
