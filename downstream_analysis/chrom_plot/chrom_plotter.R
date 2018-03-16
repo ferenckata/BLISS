52066aceb4f254bdae188e80d1d85fde83fb0562
ferenckata <35810660+ferenckata@users.noreply.github.com>
2018-03-16 15:58:08 -0700

    add karyogram

--- downstream_analysis/chrom_plot/chrom_plotter.R
+++ downstream_analysis/chrom_plot/chrom_plotter.R
@@ -2,6 +2,7 @@
 # http://bioconductor.org/packages/release/bioc/vignettes/TitanCNA/inst/doc/TitanCNA.pdf
 # based on library(TitanCNA)
 # library(argparser)
+library("SNPchip")
 
 inpath = ''
 outpath = '' # should be different from inpath
@@ -22,10 +23,13 @@ for(infile in list.files(inpath)){
   print("Plotting...")
   png(outfile)
   par(mar = c(spacing, 8, 2, 2))
+  # plotting DSBs
   plot(dataByChr$V2,dataByChr$V4,pch=16,xaxt = "n",
       las = 1,ylab = "DSB counts",xlim = c(1,chrlen),
       cex = 0.25,ylim=c(0,max(dataByChr$V4)),
       xlab="",main=chrname)
+  # plotting karyogram
+  pI <- plotIdiogram(chromosome = "1",build = "hg19",unit = "bp",label.y = -0.35,new=F,ylim=c(-15,-40),label.cytoband = F)
   dev.off()
   sprintf("Plotting of %s is done", chrname)
 }
