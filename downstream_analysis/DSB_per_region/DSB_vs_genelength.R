c6e44376adca04adc07d4d656c8096bdf69fdc64
ferenckata <35810660+ferenckata@users.noreply.github.com>
2018-05-03 18:01:48 -0700

    update and organize
    define outliers with boxplot, separate the datasets: outliers (plenty DSB) and non-outliers (<3rd quartile of DSB count)
    calculate linear regression for both
    find GEO term for outlier genes
    keep coloring version

--- downstream_analysis/DSB_per_region/DSB_vs_genelength.R
+++ downstream_analysis/DSB_per_region/DSB_vs_genelength.R
@@ -13,38 +13,75 @@ idList = c("RM118","RM119","RM120","RM121")
 
 i = 0
 
-# # # # # # # # INDIVIDUAL DATASETS # # # # # # #
-
-png(paste(outpath,"all_dsb_vs_genelength.png",sep=""))
-par(mfrow=c(3,2)) # depending on the number of datasets
-
+par(mfrow=c(1,1))
+i=0
 for(infile in files){
   i = i+1
-  
   # read non merged files and store the counts per gene
   runid = unlist(strsplit(infile,"_",fixed = T))[1]
-  print(runid)
   nm = paste(gepath,runid,"_c_gene.bed",sep="")
   
   nmc = read.delim(nm,header = F,sep="\t",as.is = T)
-  #colnm = dsbCol[runid]
+  colnm = dsbCol[runid]
   
+  print(summary(nmc$V7))
+  thrh = quantile(nmc$V7,probs = 0.75)
+  
+  pdf(paste(runid,"_boxplot.pdf",sep=""))
+  par(mfrow=c(1,1))
   # plotting the number of DSBs vs the gene length
-
-    plot(nmc[which(nmc$V7>0),3]-nmc[which(nmc$V7>0),2],nmc[which(nmc$V7>0),7],
-         ylab = "DBS counts per gene",xlab = "gene length",main = runid,
-         ylim=c(0,24000),pch=20,cex=0.5)
+  boxplot(nmc$V7/abs((nmc$V3-nmc$V2)),main=runid,
+          data=nmc,names=runid,col = colnm)
+  dev.off()
+  
+  # find outliers, plot them and calculate linear regression for them
+  outlier = nmc[which(nmc$V7>thrh),]
+  sprintf("There are %s outliers in %s dataset.",as.character(dim(outlier)[1]),runid)
+  
+  print(summary(outlier$V7))
+  minth = quantile(outlier$V7,probs = 0)
+  
+  pdf(paste(runid,"_scatterplot.pdf",sep = ""))
+  par(mfrow=c(2,1))
+  
+  plot(nmc[which(nmc$V7<minth),3]-nmc[which(nmc$V7<minth),2],nmc[which(nmc$V7<minth),7],
+       ylab = "DBS counts per gene",xlab = "gene length",main = runid,pch=20,cex=0.5)
+  
+  # abline(lm(nmc[which(nmc$V7<minth),3]-nmc[which(nmc$V7<minth),2]~nmc[which(nmc$V7<minth),7]),col="red")
+  # NOTE : the abline seems a good idea, but the values around 0 are usually so many that it looks weird
+  # better to use scatter.smooth that also plots the dots
+  # 
+  
+  plot(outlier$V3-outlier$V2,outlier$V7,
+       ylab = "DBS counts per gene",xlab = "gene length",main = runid,pch=20,cex=0.5)
+  abline(lm(outlier$V3-outlier$V2 ~ outlier$V7),col="red")
+  
+  dev.off()
+  
   # calculate correlation
-  print("the correlation is:")
-  print(cor(nmc[which(nmc$V7>0),3]-nmc[which(nmc$V7>0),2], nmc[which(nmc$V7>0),7]))
-  # build linear regression model
-  dsbgenel = lm(nmc[which(nmc$V7>0),3]-nmc[which(nmc$V7>0),2] ~ nmc[which(nmc$V7>0),7])
-  print(summary(dsbgenel))
-}  
-dev.off()
+  sprintf("The correlation between DSB number and gene length in the outlier genes is %f",
+         cor(outlier$V3-outlier$V2,outlier$V7))
+  # report linear regression model
+  print(summarylm(outlier$V3-outlier$V2 ~ outlier$V7))
+  
+  # get GO terms for the outlier genes
+  mymart = useMart("ensembl","hsapiens_gene_ensembl")
+  listAttributes(mymart)
+  goforols = getBM(attributes = c('hgnc_symbol','name_1006'),
+                  filters = 'hgnc_symbol',
+                   values = outlier$V4,
+                   mart = mymart)
+  write.table(goforols,file = "fragile_gene_GO.tsv",quote = F,sep = "\t",row.names = F,col.names = c("gene symbols","GO term"))
+  # sort by GO frequency
+  gofreq = count(goforols,vars = 'name_1006')
+  gofreq = gofreq[order(-gofrec$freq),]
+  # save GO counts
+  write.table(gofreq,file="fragile_gene_GOfreq.tsv",quote = F,sep='\t',row.names = F,col.names = T)
+}
 
 
 # # # If you prefer colored dots on top of each other for comparison  # # #
+
 # one can include more colors here and more IDs in the idList if there are more than 5 datasets
 dsbCol = c("darkgreen","palegreen","gold","steelblue3","yellow")
 names(dsbCol) = idList
@@ -62,7 +99,7 @@ for(infile in files){
   # plotting the number of DSBs vs the gene length
   
   if(i==1){
-    png(paste(outpath,"all_dsb_vs_genelength.png",sep=""))
+    pdf(paste(outpath,"all_dsb_vs_genelength.pdf",sep=""))
     plot(nmc[which(nmc$V7>0),3]-nmc[which(nmc$V7>0),2],nmc[which(nmc$V7>0),7],
          ylab = "DBS counts per gene",xlab = "gene length",main = "DSB vs gene length",
          ylim=c(0,24000),col= colnm,pch=20,cex=0.5)
@@ -72,86 +109,6 @@ for(infile in files){
   }
 }  
 dev.off()
-
-# # # If you see outliers, they can be found with boxplot # # #
-# ---------- boxplot ---------------
-par(mfrow=c(2,2))
-i=0
-for(infile in files){
-  i = i+1
-  # read non merged files and store the counts per gene
-  runid = unlist(strsplit(infile,"_",fixed = T))[1]
-  nm = paste(gepath,runid,"_c_gene.bed",sep="")
-  
-  nmc = read.delim(nm,header = F,sep="\t",as.is = T)
-  colnm = dsbCol[runid]
-  
-  # plotting the number of DSBs vs the gene length
-  # png(paste(userpath,"all_dsb_vs_genelength.png",sep=""))
-  boxplot(nmc[which(nmc$V7>0),7]/abs((nmc[which(nmc$V7>0),3]-nmc[which(nmc$V7>0),2])),data=nmc,names=runid,col = colnm,ylim=c(0,15))
-}
-
-# # # # # # POOLED DATASETS # # # # # # #
-
-userpath = "~/Documents/bliss/MCF7/MCF7_BICRO63/rtest/exon_gene_counts/"
-i=0
-# read in all files
-for(infile in list.files(userpath,pattern = "_c_gene.bed")){
-  i = i+1
-  if(i==1){
-    nm1 = read.delim(paste(userpath,infile,sep=""),header = F,sep="\t",as.is = T)
-    print("hey")
-  }else if(i==2){
-    nm2 = read.delim(paste(userpath,infile,sep=""),header = F,sep="\t",as.is = T)
-  }else if(i==3){
-    nm3 = read.delim(paste(userpath,infile,sep=""),header = F,sep="\t",as.is = T)
-  }else if(i==4){
-    nm4 = read.delim(paste(userpath,infile,sep=""),header = F,sep="\t",as.is = T)
-  }
-}
-# create an empty data frame to store the location, gene symbol and DSB counts
-nmall = cbind(nm1$V1,nm1$V2,nm1$V3,nm1$V4,0)
-
-# merge the counts from each
-for(n in 1:dim(nm1)[1]){
-  nmall[n,5] = sum(nm1$V7[n],nm2$V7[n],nm3$V7[n],nm4$V7[n])
-}
-
-nmalldf = data.frame(nmall)
-
-# make scatterplot
-png("all_DSB_vs_genelength.png",width=600,height=700)
-plot(as.integer(nmall[which(as.integer(nmall[,5])>0),3])-as.integer(nmall[which(as.integer(nmall[,5])>0),2]),
-     as.integer(nmall[which(as.integer(nmall[,5])>0),5]),ylab = "DBS counts per gene",
-     xlab = "gene length",main = "DSB vs gene length",pch=16)
-dev.off()
-# plot without outliers (genes with >20 000 DSB)
-png("all_DSB_vs_genelength_no_outl.png",width=600,height=700)
-plot(as.integer(nmall[which(as.integer(nmall[,5])>0 & as.integer(nmall[,5])<20000),3])-as.integer(nmall[which(as.integer(nmall[,5])>0 & as.integer(nmall[,5])<20000),2]),
-     as.integer(nmall[which(as.integer(nmall[,5])>0 & as.integer(nmall[,5])<20000),5]),ylab = "DBS counts per gene",
-     xlab = "gene length",main = "DSB vs gene length",pch=16)
-dev.off()
-
-# set a limit and pick all genes above that
-outliers = nmall[which(as.integer(nmall[,5])>20000),4]
-
-for(i in 1:length(outliers)){
-  outliers[i] = unlist(strsplit(outliers[i],";",fixed=T)[1])[1]
-}
-
-# get GO terms for these
-mymart = useMart("ensembl","hsapiens_gene_ensembl")
-listAttributes(mymart)
-goforols = getBM(attributes = c('hgnc_symbol','name_1006'),
-                 filters = 'hgnc_symbol',
-                 values = outliers,
-                 mart = mymart)
-write.table(goforols,file = "fragile_gene_GO.tsv",quote = F,sep = "\t",row.names = F,col.names = c("gene symbols","GO term"))
-# sort by GO frequency
-gofreq = count(goforols,vars = 'name_1006')
-gofreq = gofreq[order(-gofrec$freq),]
-# save GO counts
-write.table(gofreq,file="fragile_gene_GOfreq.tsv",quote = F,sep='\t',row.names = F,col.names = T)
 
   
   
